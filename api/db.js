9a69564bb674d5b2650a902643953ddf7a3e475f
geetansh170 <geetanshkumar170@gmail.com>
2020-01-24 00:41:04 +0530

    login page front end

--- api/db.js
+++ api/db.js
@@ -1,22 +1,17 @@
- const {
-     MongoClient
- } = require('mongodb');
- const {
-     mongodbUrl
- } = require('./config');
+const { MongoClient } = require("mongodb");
+const { mongodbUrl } = require("./config");
 
+exports.connect = (cb) => {
+  //Mongodb version 3
 
- exports.connect = (cb) => {
-
-     //Mongodb version 3
-
-     MongoClient.connect(mongodbUrl, {
-         useUnifiedTopology: true,
-         useNewUrlParser: true
-     }, (err, client) => {
-
-         return cb(err, client);
-
-
-     });
- };
+  MongoClient.connect(
+    mongodbUrl,
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    },
+    (err, client) => {
+      return cb(err, client);
+    }
+  );
+};
--- web/src/components/pages/user/login.js
+++ web/src/components/pages/user/login.js
@@ -1,128 +1,230 @@
-import React, {Component} from 'react';
-import styled from 'styled-components'
-import {history} from "../../../history";
+import React, { Component } from "react";
+import styled from "styled-components";
+import { history } from "../../../history";
 import {
-    FormSuccessMessage,
-    FormErrorMessage,
-    Form,
-    FormItem,
-    FormAction,
-    FormInput,
-    FormLabel,
-    FormSubmit
-} from '../../themes/form'
+  FormSuccessMessage,
+  FormErrorMessage,
+  Form,
+  FormItem,
+  FormAction,
+  FormInput,
+  FormLabel,
+  FormSubmit
+} from "../../themes/form";
 
-import _ from 'lodash'
+import _ from "lodash";
 
-const LoginWrapper = styled.div `
-
-`
-
-
+const LoginWrapper = styled.div``;
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      message: {
+        type: "success",
+        msg: null
+      },
+      user: {
+        email: "",
+        password: ""
+      }
+    };
 
-    constructor(props) {
-        super(props);
+    this._onSubmit = this._onSubmit.bind(this);
+    this._onTextFieldChange = this._onTextFieldChange.bind(this);
+  }
 
-        this.state = {
-            message: {
-                type: 'success',
-                msg: null
-            },
-            user: {
-                email: "",
-                password: "",
-            }
-        }
+  UNSAFE_componentWillMount() {
+    const { store } = this.props;
 
-        this._onSubmit = this._onSubmit.bind(this);
-        this._onTextFieldChange = this._onTextFieldChange.bind(this);
+    const currentUser = store.getCurrentUser();
+    if (currentUser) {
+      // user is logged in we need redirect him to other page.
+
+      history.push("/");
     }
+  }
 
-    UNSAFE_componentWillMount() {
+  _onTextFieldChange(event) {
+    let { user } = this.state;
 
-        const {store} = this.props;
+    const field = event.target.name;
+    const value = event.target.value;
 
-        const currentUser = store.getCurrentUser();
-        if(currentUser){
-            // user is logged in we need redirect him to other page.
+    user[field] = value;
 
-            history.push('/');
-        }
+    this.setState({
+      user: user
+    });
+  }
 
-    }
+  _onSubmit(event) {
+    const { user } = this.state;
+    const { store } = this.props;
 
-    _onTextFieldChange(event) {
+    event.preventDefault();
 
-        let {user} = this.state;
+    console.log("FOrm is submitted with value", user);
 
-        const field = event.target.name;
-        const value = event.target.value;
-
-        user[field] = value;
-
+    store.login(user, (err, result) => {
+      if (err) {
         this.setState({
-            user: user
+          message: {
+            type: "error",
+            msg: _.get(err, "response.data.error.message", "Login Error")
+          }
         });
-
-    }
-
-    _onSubmit(event) {
-        const {user} = this.state;
-        const {store} = this.props;
-
-        event.preventDefault();
-
-        console.log("FOrm is submitted with value", user)
-
-        store.login(user, (err, result) => {
-
-            if (err) {
-
-                this.setState({
-                    message: {type: 'error', msg: _.get(err, 'response.data.error.message', "Login Error")}
-                })
-            } else {
-
-                this.setState({
-                    message: {type: 'success', msg: 'Login successful.'}
-                })
-            }
-
+      } else {
+        this.setState({
+          message: { type: "success", msg: "Login successful." }
         });
-    }
+      }
+    });
+  }
 
-    render() {
+  render() {
+    const { user, message } = this.state;
 
-        const {user, message} = this.state;
-
-        return (
-            <LoginWrapper>
-                <h2>Sign In</h2>
-                <Form onSubmit={this._onSubmit}>
-                    {
-                        message.msg ? message.type === 'success' ?
-                            <FormSuccessMessage>{message.msg}</FormSuccessMessage> :
-                            <FormErrorMessage>{message.msg}</FormErrorMessage> : null
-                    }
-                    <FormItem>
-                        <FormLabel>Email</FormLabel>
-                        <FormInput onChange={this._onTextFieldChange} value={_.get(user, 'email', '')} type={'email'}
-                                   name={'email'} placeholder={'Your email address'}/>
-                    </FormItem>
-                    <FormItem>
-                        <FormLabel>Password</FormLabel>
-                        <FormInput onChange={this._onTextFieldChange} value={_.get(user, 'password', '')}
-                                   type={'password'} name={'password'} placeholder={'Password'}/>
-                    </FormItem>
-                    <FormAction>
-                        <FormSubmit type={"submit"}>Login</FormSubmit>
-                    </FormAction>
-                </Form>
-            </LoginWrapper>
-        )
-
-    }
+    return (
+      // <LoginWrapper>
+      //     <h2>Sign In</h2>
+      //     <Form onSubmit={this._onSubmit}>
+      //         {
+      //             message.msg ? message.type === 'success' ?
+      //                 <FormSuccessMessage>{message.msg}</FormSuccessMessage> :
+      //                 <FormErrorMessage>{message.msg}</FormErrorMessage> : null
+      //         }
+      //         <FormItem>
+      //             <FormLabel>Email</FormLabel>
+      //             <FormInput onChange={this._onTextFieldChange} value={_.get(user, 'email', '')} type={'email'}
+      //                        name={'email'} placeholder={'Your email address'}/>
+      //         </FormItem>
+      //         <FormItem>
+      //             <FormLabel>Password</FormLabel>
+      //             <FormInput onChange={this._onTextFieldChange} value={_.get(user, 'password', '')}
+      //                        type={'password'} name={'password'} placeholder={'Password'}/>
+      //         </FormItem>
+      //         <FormAction>
+      //             <FormSubmit type={"submit"}>Login</FormSubmit>
+      //         </FormAction>
+      //     </Form>
+      // </LoginWrapper>
+      <div>
+        <title>Login V16</title>
+        <meta charSet='UTF-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        {/*===============================================================================================*/}
+        <link rel='icon' type='image/png' href='images/icons/favicon.ico' />
+        {/*===============================================================================================*/}
+        <link
+          rel='stylesheet'
+          type='text/css'
+          href='vendor/bootstrap/css/bootstrap.min.css'
+        />
+        {/*===============================================================================================*/}
+        <link
+          rel='stylesheet'
+          type='text/css'
+          href='fonts/font-awesome-4.7.0/css/font-awesome.min.css'
+        />
+        {/*===============================================================================================*/}
+        <link
+          rel='stylesheet'
+          type='text/css'
+          href='fonts/Linearicons-Free-v1.0.0/icon-font.min.css'
+        />
+        {/*===============================================================================================*/}
+        <link
+          rel='stylesheet'
+          type='text/css'
+          href='vendor/animate/animate.css'
+        />
+        {/*===============================================================================================*/}
+        <link
+          rel='stylesheet'
+          type='text/css'
+          href='vendor/css-hamburgers/hamburgers.min.css'
+        />
+        {/*===============================================================================================*/}
+        <link
+          rel='stylesheet'
+          type='text/css'
+          href='vendor/animsition/css/animsition.min.css'
+        />
+        {/*===============================================================================================*/}
+        <link
+          rel='stylesheet'
+          type='text/css'
+          href='vendor/select2/select2.min.css'
+        />
+        {/*===============================================================================================*/}
+        <link
+          rel='stylesheet'
+          type='text/css'
+          href='vendor/daterangepicker/daterangepicker.css'
+        />
+        {/*===============================================================================================*/}
+        <link rel='stylesheet' type='text/css' href='css/util.css' />
+        <link rel='stylesheet' type='text/css' href='css/main.css' />
+        {/*===============================================================================================*/}
+        <div className='limiter'>
+          <div
+            className='container-login100'
+            style={{ backgroundImage: 'url("images/bg-01.jpg")' }}
+          >
+            <div className='wrap-login100 p-t-30 p-b-50'>
+              <span className='login100-form-title p-b-41'>Account Login</span>
+              <form
+                className='login100-form validate-form p-b-33 p-t-5'
+                onSubmit={this._onSubmit}
+              >
+                <div
+                  className='wrap-input100 validate-input'
+                  data-validate='Enter username'
+                >
+                  <input
+                    onChange={this._onTextFieldChange}
+                    value={_.get(user, "email", "")}
+                    type={"email"}
+                    className='input100'
+                    name='username'
+                    placeholder='User name'
+                  />
+                  <span className='focus-input100' data-placeholder='' />
+                </div>
+                <div
+                  className='wrap-input100 validate-input'
+                  data-validate='Enter password'
+                >
+                  <input
+                    onChange={this._onTextFieldChange}
+                    value={_.get(user, "password", "")}
+                    className='input100'
+                    type='password'
+                    name='pass'
+                    placeholder='Password'
+                  />
+                  <span className='focus-input100' data-placeholder='' />
+                </div>
+                <div className='container-login100-form-btn m-t-32'>
+                  <button className='login100-form-btn' type={"submit"}>
+                    Login
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div id='dropDownSelect1' />
+        {/*===============================================================================================*/}
+        {/*===============================================================================================*/}
+        {/*===============================================================================================*/}
+        {/*===============================================================================================*/}
+        {/*===============================================================================================*/}
+        {/*===============================================================================================*/}
+        {/*===============================================================================================*/}
+      </div>
+    );
+  }
 }
